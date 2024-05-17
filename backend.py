caf3ecf8a531689bd4c425a5d264ccc2e2d98eeb
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-17 20:38:29 +0200

    Add dictionary to filter temp and sky conditions.

--- backend.py
+++ backend.py
@@ -6,7 +6,16 @@ def get_data(place, forecast_days=None, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+
+    if kind == "Temperature":
+        filtered_data = [d["main"]["temp"] for d in filtered_data]
+    elif kind == "Sky":
+        filtered_data = [d["weather"][0]["main"] for d in filtered_data]
+
+    return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
